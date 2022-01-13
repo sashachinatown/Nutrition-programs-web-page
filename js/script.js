a80ee036706fc2d6741522f622949a142c756a2c
Oleksandr <kulibaba99@gmail.com>
2022-01-13 11:00:14 +0200

    menu cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*jshint multistr: true */
 window.addEventListener('DOMContentLoaded', () => {
 
     // Tabs
@@ -131,7 +132,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 5000);
+    // const modalTimerId = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
 
@@ -144,5 +145,76 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // Карточки (Используем классы)
+
+    class MenuCards {
+        constructor(imgSrc, alt, title, text, price, parentSelector) {
+            this.imgSrc = imgSrc;
+            this.alt = alt;
+            this.title = title;
+            this.text = text;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+        
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.imgSrc} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.text}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCards(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и \
+        фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной \
+        ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+    
+    new MenuCards(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и \
+        качественное исполнение блюд. Красная рыба, морепродукты, фрукты - \
+        ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container'
+    ).render();
+
+    new MenuCards(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню "Меню “Постное” - это тщательный подбор ингредиентов: полное \
+        отсутствие продуктов животного происхождения, молоко из миндаля, овса, \
+        кокоса или гречки, правильное количество белков за счет тофу и импортных \
+        вегетарианских стейков.',
+        21,
+        '.menu .container'
+    ).render();
+
 
 });
