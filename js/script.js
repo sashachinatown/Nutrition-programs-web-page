283d2e30249f050cbe1b98cd50838581de1960e8
Oleksandr <kulibaba99@gmail.com>
2022-06-19 15:07:41 +0300

    timer fix

--- js/script.js
+++ js/script.js
@@ -42,14 +42,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2022-02-20';
+    const deadline = '2022-06-20';
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        let days = 0, hours = 0, minutes = 0, seconds = 0;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t > 0) {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
         
         return {
             'total': t,
